660a18c3389be2c7192adb530fe89381cfb7959f
jessicalee <jessicalee@via.com.tw>
2023-06-08 15:38:15 +0800

    import api code added to fall detection

--- compiler_and_runtime/Neuropl/boostTest.py
+++ compiler_and_runtime/Neuropl/boostTest.py
@@ -6,7 +6,7 @@ import numpy as np
 
 blank_image = np.zeros((224,224,3), np.uint8)
 cvImg = np.zeros((224, 224, 3), dtype = "uint8")
-#print(hello_ext.greet())
+# print(hello_ext.greet())
 # t = boostClass.Test("Hello")
 # t.scream()
 # print(t.myMax(12,15))
