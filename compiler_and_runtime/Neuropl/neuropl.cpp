2374be563fdb1bf1f93f47c6c738ccaab73440b3
Sylvia Chen <sylvia2002chen@gmail.com>
2023-06-26 09:57:12 +0800

    improved code quality

--- compiler_and_runtime/Neuropl/neuropl.cpp
+++ compiler_and_runtime/Neuropl/neuropl.cpp
@@ -3,7 +3,7 @@
 namespace bp = boost::python;
 namespace np = boost::python::numpy;
 
-/* Disables error messages */
+// Disables error messages
 int disable(FILE* stream)
 {
     int saved = dup(fileno(stream));
@@ -14,20 +14,19 @@ int disable(FILE* stream)
     return saved;
 }
 
-/* Restores error messages */
+// Restores error messages
 void restore(FILE* stream, int saved)
 {
     dup2(saved, fileno(stream));
     close(saved);
 }
 
-/* Constructor function. */
-Neuropl::Neuropl(std::string path){ 
-
-    /* Setup NeuroRuntime */
+Neuropl::Neuropl(std::string path)
+{
     model_path = path;
     Py_Initialize();
     np::initialize();
+
     // Open the shared library
     handle = dlopen("/usr/lib/libneuron_runtime.so", RTLD_LAZY);
     if (handle == nullptr) {
@@ -35,8 +34,9 @@ Neuropl::Neuropl(std::string path){
     }
 
     // Load all the NeuroRuntime functions
-    #define LOAD_FUNCTIONS(FUNC_NAME, VARIABLE_NAME) \
-        VARIABLE_NAME = reinterpret_cast<FUNC_NAME>(load_func(handle, #FUNC_NAME));
+
+#define LOAD_FUNCTIONS(FUNC_NAME, VARIABLE_NAME) \
+    VARIABLE_NAME = reinterpret_cast<FUNC_NAME>(load_func(handle, #FUNC_NAME));
 
     LOAD_FUNCTIONS(NeuronRuntime_create, rt_create);
     LOAD_FUNCTIONS(NeuronRuntime_loadNetworkFromFile, loadNetworkFromFile);
@@ -52,25 +52,26 @@ Neuropl::Neuropl(std::string path){
     LOAD_FUNCTIONS(NeuronRuntime_getOutputSize, getOutputSize);
     LOAD_FUNCTIONS(NeuronRuntime_getSingleOutputSize, getSingleOutputSize);
     LOAD_FUNCTIONS(NeuronRuntime_getProfiledQoSData, getProfiledQoSData);
-    LOAD_FUNCTIONS(NeuronRuntime_getOutputPaddedDimensions, getOutputPaddedDimensions);
+    LOAD_FUNCTIONS(NeuronRuntime_getOutputPaddedDimensions,
+        getOutputPaddedDimensions);
 
     // Setup the environment options for the Neuron Runtime
     EnvOptions envOptions = {
         .deviceKind = kEnvOptHardware,
-        .MDLACoreOption = 1,  // Force single MDLA
-        .suppressInputConversion = false,  // Please refer to the explanation above
-        .suppressOutputConversion = false,  // Please refer to the explanation above
+        .MDLACoreOption = 1, // Force single MDLA
+        .suppressInputConversion = false, // Please refer to the explanation above
+        .suppressOutputConversion = false, // Please refer to the explanation above
     };
 
     // Setup the QoS options for the Neuron Runtime
     // Please refer to RuntimeAPI.h above for details
     QoSOptions qosOptions = {
-        NEURONRUNTIME_PREFER_PERFORMANCE,  // Preference
-        NEURONRUNTIME_PRIORITY_MED,  // Priority
-        NEURONRUNTIME_BOOSTVALUE_MAX,  // Boost value
-        0,  // Deadline
-        0,  // Abort time
-        nullptr,  // Profiled QoS Data
+        NEURONRUNTIME_PREFER_PERFORMANCE, // Preference
+        NEURONRUNTIME_PRIORITY_MED, // Priority
+        NEURONRUNTIME_BOOSTVALUE_MAX, // Boost value
+        0, // Deadline
+        0, // Abort time
+        nullptr, // Profiled QoS Data
     };
 
     // Create neuron runtime environment
@@ -90,11 +91,12 @@ Neuropl::Neuropl(std::string path){
     if (err_code != NEURONRUNTIME_NO_ERROR) {
         throw std::runtime_error("Failed to get single input size for network.");
     }
-    std::cout << "The required size of the input buffer is " << input_size << std::endl;
+    std::cout << "The required size of the input buffer is " << input_size
+              << std::endl;
 
-    BufferAttribute attr {-1};
-    //Set the output buffer
-    for(int i = 0; i < 10; i++){
+    BufferAttribute attr { -1 };
+    // Set the output buffer
+    for (int i = 0; i < 10; i++) {
         size_t size;
         int fd = disable(stderr);
         err_code = (*getOutputSize)(runtime, i, &size);
@@ -103,50 +105,51 @@ Neuropl::Neuropl(std::string path){
         }
         output_buf.push_back(std::vector<uint8_t>(size));
         restore(stderr, fd);
-        err_code = (*setOutput)(runtime, i, static_cast<void *>(output_buf[i].data()), size, attr);
+        err_code = (*setOutput)(
+            runtime, i, static_cast<void*>(output_buf[i].data()), size, attr);
         if (err_code != NEURONRUNTIME_NO_ERROR) {
             throw std::runtime_error("Failed to set single output for network.");
         }
     }
-
 }
 
-/*Destructor function */
-Neuropl::~Neuropl() {
+Neuropl::~Neuropl()
+{
     // Releases runtime resources
     (*release)(runtime);
 }
 
-/* Function implementations */
-
-/* This function converts a vector<vector<usigned char>> into a python list of ndarrays. */
-bp::list convertToListOfNdarrays(const std::vector<std::vector<unsigned char>>& data)
+// This function converts a vector<vector<usigned char>> into a python list of
+// ndarrays.
+bp::list convertToListOfNdarrays(
+    const std::vector<std::vector<uint8_t>>& data)
 {
-    typename std::vector<std::vector<unsigned char>>::const_iterator iter;
+    typename std::vector<std::vector<uint8_t>>::const_iterator iter;
     boost::python::list list;
     for (iter = data.begin(); iter != data.end(); ++iter) {
-        np::ndarray ndarray = np::zeros(bp::make_tuple((*iter).size()), np::dtype::get_builtin<unsigned char>());
-        for (std::size_t i = 0; i < (*iter).size(); ++i)
-        {
-            *reinterpret_cast<unsigned char*>(ndarray.get_data() + i * ndarray.strides(0)) = (*iter)[i];
+        np::ndarray ndarray = np::zeros(bp::make_tuple((*iter).size()),
+            np::dtype::get_builtin<uint8_t>());
+        for (std::size_t i = 0; i < (*iter).size(); ++i) {
+            *reinterpret_cast<uint8_t*>(ndarray.get_data() + i * ndarray.strides(0)) = (*iter)[i];
         }
         list.append(ndarray);
     }
     return list;
 }
 
-/* For C++ users */
-const std::vector<std::vector<uint8_t>> & Neuropl::predict(uint8_t* byte_buffer)
+const std::vector<std::vector<uint8_t>>& Neuropl::predict(
+    uint8_t* byte_buffer)
 {
     // Check for invalid arguement
-    if(byte_buffer == nullptr){
+    if (byte_buffer == nullptr) {
         throw std::invalid_argument("byte_buffer is nullptr");
-    }   
+    }
 
     int err_code;
     // Set the input buffer with our memory buffer (pixels inside)
-    BufferAttribute attr {-1};
-    err_code = (*setSingleInput)(runtime, static_cast<void *>(byte_buffer), input_size, attr);
+    BufferAttribute attr { -1 };
+    err_code = (*setSingleInput)(runtime, static_cast<void*>(byte_buffer),
+        input_size, attr);
     if (err_code != NEURONRUNTIME_NO_ERROR) {
         throw std::runtime_error("Failed to set single input for network.");
     }
@@ -158,59 +161,64 @@ const std::vector<std::vector<uint8_t>> & Neuropl::predict(uint8_t* byte_buffer)
     }
 
     // Get profiled QoS Data
-    // Neuron Rutime would allocate ProfiledQoSData instance when the input profiledQoSData is nullptr.
+    // Neuron Rutime would allocate ProfiledQoSData instance when the input
+    // profiledQoSData is nullptr.
     ProfiledQoSData* profiledQoSData = nullptr;
     uint8_t executingBoostValue;
     err_code = (*getProfiledQoSData)(runtime, &profiledQoSData, &executingBoostValue);
     if (err_code != NEURONRUNTIME_NO_ERROR) {
         std::cerr << "Failed to Get QoS Data" << std::endl;
     }
-    
+
     // Print out profiled QoS Data and executing boost value
     if (profiledQoSData != nullptr) {
         std::cout << "Dump the profiled QoS Data:" << std::endl;
-        std::cout << "executing boost value = " << +executingBoostValue << std::endl;
+        std::cout << "executing boost value = " << +executingBoostValue
+                  << std::endl;
         for (uint32_t i = 0u; i < profiledQoSData->numSubgraph; ++i) {
             for (uint32_t j = 0u; j < profiledQoSData->numSubCmd[i]; ++j) {
                 std::cout << "SubCmd[" << i << "][" << j << "]:" << std::endl;
-                std::cout << "execution time = " << profiledQoSData->qosData[i][j].execTime << std::endl;
-                std::cout << "boost value = " << +profiledQoSData->qosData[i][j].boostValue << std::endl;
-                std::cout << "bandwidth = " << profiledQoSData->qosData[i][j].bandwidth << std::endl;
+                std::cout << "execution time = "
+                          << profiledQoSData->qosData[i][j].execTime << std::endl;
+                std::cout << "boost value = "
+                          << +profiledQoSData->qosData[i][j].boostValue << std::endl;
+                std::cout << "bandwidth = " << profiledQoSData->qosData[i][j].bandwidth
+                          << std::endl;
             }
         }
     } else {
         std::cerr << "profiledQoSData is nullptr" << std::endl;
     }
 
-	return output_buf;
-    
+    return output_buf;
 }
 
-/* For Python users */
-bp::list Neuropl::predict(np::ndarray data) {    
+bp::list Neuropl::predict(np::ndarray data)
+{
     // Verify input size
     np::ndarray npArray = bp::extract<np::ndarray>(data);
     int array_size = 1;
-    for(int i = 0; i < data.get_nd(); i++){
-        array_size*= (int)(npArray.shape(i));
+    for (int i = 0; i < data.get_nd(); i++) {
+        array_size *= (int)(npArray.shape(i));
     }
 
-    if(array_size != input_size){
-        std::cerr << "Actual input size =  "<< array_size  <<"Expected input size = "<< input_size<< std::endl;
+    if (array_size != input_size) {
+        std::cerr << "Actual input size =  " << array_size
+                  << "Expected input size = " << input_size << std::endl;
         throw std::invalid_argument("Input dimension mismatch.");
     }
 
     // Get the buffer pointer and size
     uint8_t* byte_buffer = reinterpret_cast<uint8_t*>(npArray.get_data());
     auto ret = Neuropl::predict(byte_buffer);
+
     // Convert ret from vector vector to 2d list of numpy arrays
-    auto retNumpy = convertToListOfNdarrays(ret);
-    return retNumpy;
+    return convertToListOfNdarrays(ret);
 }
 
-void *  Neuropl::load_func(void * handle, const char * func_name) {
-    /* Load the function specified by func_name, and exit if the loading failed. */
-    void * func_ptr = dlsym(handle, func_name); /* Find the run-time address in the shared object HANDLE refers to of the symbol called NAME. */
+void* Neuropl::load_func(void* handle, const char* func_name)
+{
+    void* func_ptr = dlsym(handle, func_name);
 
     if (func_name == nullptr) {
         std::cerr << "Find " << func_name << " function failed." << std::endl;
@@ -219,14 +227,11 @@ void *  Neuropl::load_func(void * handle, const char * func_name) {
     return func_ptr;
 }
 
-/* Export using Boost.Python */
 BOOST_PYTHON_MODULE(neuropl)
 {
     using namespace boost::python;
 
     bp::list (Neuropl::*predict)(np::ndarray data) = &Neuropl::predict;
 
-    class_<Neuropl>("Neuropl",  init<std::string>())
-         .def("predict", predict)
-        ;
+    class_<Neuropl>("Neuropl", init<std::string>()).def("predict", predict);
 }
--- compiler_and_runtime/Neuropl/neuropl.h
+++ compiler_and_runtime/Neuropl/neuropl.h
@@ -1,28 +1,28 @@
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "RuntimeAPI.h"
-#include <string>
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <fstream>
-#include <dlfcn.h>
-#include <getopt.h>
-#include <cstdlib>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include <boost/python.hpp>
 #include <boost/python/numpy.hpp>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+#include "RuntimeAPI.h"
 
 namespace bp = boost::python;
 namespace np = boost::python::numpy;
 
-
-class Neuropl{
+class Neuropl {
 public:
     /* The constructor function */
     Neuropl(std::string path);
@@ -30,37 +30,54 @@ public:
     ~Neuropl();
 
     /* Functions */
-    bp::list predict (np::ndarray data);
-    const std::vector<std::vector<uint8_t>> &predict(uint8_t* byte_buffer);
+    bp::list predict(np::ndarray data);
+    const std::vector<std::vector<uint8_t>>& predict(uint8_t* byte_buffer);
 
 private:
-    
     std::string model_path;
 
     size_t input_size;
     void* runtime;
-    void * handle;
+    void* handle;
     std::vector<std::vector<uint8_t>> output_buf;
     /* Should be called once per neuropl initialization. */
-    void * load_func(void * handle, const char * func_name);
+    void* load_func(void* handle, const char* func_name);
 
     /* typedef all the NeuronRuntime library functions */
-    typedef int (*NeuronRuntime_create)(const EnvOptions* options, void** runtime);
-    typedef int (*NeuronRuntime_loadNetworkFromFile)(void* runtime, const char* pathToDlaFile);
-    typedef int (*NeuronRuntime_loadNetworkFromBuffer)(void* runtime, const void* buffer, size_t size);
-    typedef int (*NeuronRuntime_setInput)(void* runtime, uint64_t handle, const void* buffer, size_t length, BufferAttribute attr);
-    typedef int (*NeuronRuntime_setSingleInput)(void* runtime, const void* buffer, size_t length, BufferAttribute attr);
-    typedef int (*NeuronRuntime_setOutput)(void* runtime, uint64_t handle, void* buffer, size_t length, BufferAttribute attr);
-    typedef int (*NeuronRuntime_setSingleOutput)(void* runtime, void* buffer, size_t length, BufferAttribute attr);
-    typedef int (*NeuronRuntime_setQoSOption)(void* runtime, const QoSOptions* qosOption);
-    typedef int (*NeuronRuntime_getInputSize)(void* runtime, uint64_t handle, size_t* size);
+    typedef int (*NeuronRuntime_create)(const EnvOptions* options,
+        void** runtime);
+    typedef int (*NeuronRuntime_loadNetworkFromFile)(void* runtime,
+        const char* pathToDlaFile);
+    typedef int (*NeuronRuntime_loadNetworkFromBuffer)(void* runtime,
+        const void* buffer,
+        size_t size);
+    typedef int (*NeuronRuntime_setInput)(void* runtime, uint64_t handle,
+        const void* buffer, size_t length,
+        BufferAttribute attr);
+    typedef int (*NeuronRuntime_setSingleInput)(void* runtime, const void* buffer,
+        size_t length,
+        BufferAttribute attr);
+    typedef int (*NeuronRuntime_setOutput)(void* runtime, uint64_t handle,
+        void* buffer, size_t length,
+        BufferAttribute attr);
+    typedef int (*NeuronRuntime_setSingleOutput)(void* runtime, void* buffer,
+        size_t length,
+        BufferAttribute attr);
+    typedef int (*NeuronRuntime_setQoSOption)(void* runtime,
+        const QoSOptions* qosOption);
+    typedef int (*NeuronRuntime_getInputSize)(void* runtime, uint64_t handle,
+        size_t* size);
     typedef int (*NeuronRuntime_getSingleInputSize)(void* runtime, size_t* size);
-    typedef int (*NeuronRuntime_getOutputSize)(void* runtime, uint64_t handle, size_t* size);
+    typedef int (*NeuronRuntime_getOutputSize)(void* runtime, uint64_t handle,
+        size_t* size);
     typedef int (*NeuronRuntime_getSingleOutputSize)(void* runtime, size_t* size);
-    typedef int (*NeuronRuntime_getProfiledQoSData)(void* runtime, ProfiledQoSData** profiledQoSData, uint8_t* execBoostValue);
+    typedef int (*NeuronRuntime_getProfiledQoSData)(
+        void* runtime, ProfiledQoSData** profiledQoSData,
+        uint8_t* execBoostValue);
     typedef int (*NeuronRuntime_inference)(void* runtime);
     typedef void (*NeuronRuntime_release)(void* runtime);
-    typedef int (*NeuronRuntime_getOutputPaddedDimensions)(void* runtime, uint64_t handle, RuntimeAPIDimensions* dims);
+    typedef int (*NeuronRuntime_getOutputPaddedDimensions)(
+        void* runtime, uint64_t handle, RuntimeAPIDimensions* dims);
 
     NeuronRuntime_create rt_create;
     NeuronRuntime_loadNetworkFromFile loadNetworkFromFile;
